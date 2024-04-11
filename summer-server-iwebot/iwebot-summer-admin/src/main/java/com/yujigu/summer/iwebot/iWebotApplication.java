e6adb8b3e80226870fdaac2ba1ef6ecced722d1c
liuyang <ibalbal@163.com>
2024-04-11 16:07:07 +0800

    refactor(iwebot): 启动成功发送通知

--- summer-server-iwebot/iwebot-summer-admin/src/main/java/com/yujigu/summer/iwebot/iWebotApplication.java
+++ summer-server-iwebot/iwebot-summer-admin/src/main/java/com/yujigu/summer/iwebot/iWebotApplication.java
@@ -3,6 +3,7 @@ package com.yujigu.summer.iwebot;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @EnableScheduling
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -22,11 +24,12 @@ public class iWebotApplication {
         public static void main(String[] args) {
             SpringApplication.run(iWebotApplication.class, args);
             JSONObject param = JSONUtil.createObj();
-            param.set("msg", "wxid_oa0rwmnimagm21");
-            param.set("receiver", "机器人后台启动成功");
+            param.set("msg", "机器人后台启动成功");
+            param.set("receiver", "wxid_oa0rwmnimagm21");
             Map<String,String > headers = new HashMap();
             headers.put("Authorization", "Bearer KpnJuEdJVaNpjBjXOfBmTVuXQLNtzFSNwJNJffXEuydkRKTpdHbcjrCXYwotUYocMstxaNOsSstTzJrNjZVfAJqWRPQUeccpTT");
-            HttpUtil.createPost("http://192.168.10.10:7600/wcf/send_txt").body(param.toString()).addHeaders(headers).execute().body();
+            String body = HttpUtil.createPost("http://192.168.10.10:7600/wcf/send_txt").body(param.toString()).addHeaders(headers).execute().body();
+            log.info("机器人后台启动成功:{}",body);
         }
 
 }
